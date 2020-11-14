82251bbdd0bffb17d348c2fa95a399a8777a1bb7
Malo Lebrin <malolebrin@gmail.com>
2020-11-14 19:22:18 +0100

    remove useless import

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
+const User = require('../model/User');
 
 router.post('/company-login', async (req, res) => {
     const { email, password } = req.fields;
