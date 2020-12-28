8780a311c70870495790e625adcbba01c26807b7
Malo Lebrin <malolebrin@gmail.com>
2020-12-28 14:21:22 +0100

    add delete order route

--- routes/order.js
+++ routes/order.js
@@ -8,6 +8,30 @@ const Product = require('../model/Product');
 const Order = require('../model/Order')
 const nodemailer = require('nodemailer')
 
+router.get('/orders/all', isAuthenticated, async (req, res) => {
+    try {
+        if (req.user.role === Role.User) {
+            const userOrders = await Order.find({ user: req.user._id })
+            return res.status(200).json(userOrders)
+
+        } else if (req.user.role === Role.Company) {
+            const companyOrders = await Order.find({ company: req.user._id })
+            return res.status(200).json(companyOrders)
+        }
+
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+})
+router.delete('/deleteOrder/:id', isAuthenticated, checkRole(Role.Company), async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Order.findByIdAndDelete(id)
+        return res.status(200).json({ message: 'Oder deleted successfully' })
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+})
 router.post('/new-order', isAuthenticated, checkRole(Role.User), async (req, res) => {
     try {
         const { amount, company, products } = req.fields;
@@ -31,7 +55,7 @@ router.post('/new-order', isAuthenticated, checkRole(Role.User), async (req, res
         });
         const mailOptions = {
             from: `${process.env.MAIL_ADRESS}`,
-            to: `${res.user.email}`,
+            to: `${req.user.email}`,
             subject: `Confirmation de commande ${newOrder._id}`,
             text: `votre commande a bien été envoyé à la boulangerie`
         };
